76356753e880af65cb3c41e4fe683de0bf8bdf77
tipan <carlos.tipan.rodr@gmail.com>
2024-05-25 12:43:23 +0200

    Initial Commit

---
+++ UrRecipts/UrReciptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrReciptsApp.swift
+//  UrRecipts
+//
+//  Created by Carlos Tipán on 25/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UrReciptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UrReciptsUITests/UrReciptsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UrReciptsUITestsLaunchTests.swift
+//  UrReciptsUITests
+//
+//  Created by Carlos Tipán on 25/5/24.
+//
+
+import XCTest
+
+final class UrReciptsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
